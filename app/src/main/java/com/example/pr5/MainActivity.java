732fd48ed8b9adb69b0020d0653ed926e0f00659
MatveiKylosov <matveikyl@mail.ru>
2024-04-05 23:27:45 +0500

    Разработанная практическая работа на оценку удовлетворительно. Кылосов

--- app/src/main/java/com/example/pr5/MainActivity.java
+++ app/src/main/java/com/example/pr5/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.pr5;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +16,85 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv_timer;
+    int time = 0;
+
+    Timer timer;
+    TimerTask mTimerTask;
+    boolean active = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        tv_timer = findViewById(R.id.textView2);
+    }
+
+    public void OnStart(View view){
+        active = !active;
+
+        if(active){
+            if(timer != null)
+                timer.cancel();
+
+            timer = new Timer();
+            mTimerTask = new MyTimerTask();
+
+            timer.schedule(mTimerTask, 0, 1000);
+            Button button = findViewById(R.id.button);
+            button.setText("СТОП");
+        }
+        else{
+            if(timer != null)
+                timer.cancel();
+
+            timer = null;
+
+            Button button = findViewById(R.id.button);
+            button.setText("СТОП");
+        }
+    }
+    public void onClear(){
+        if(timer != null)
+            timer.cancel();
+
+        timer = null;
+        time = 0;
+        tv_timer.setText("00:00:00");
+
+        Button button = findViewById(R.id.button);
+        button.setText("Начать");
+    }
+
+    public class MyTimerTask extends TimerTask {
+        @Override
+        public void run(){
+            String s_second = "";
+            time++;
+
+            int hour = time/60/60;
+            int min = time/60 - (hour * 60);
+            int second = time - (min * 60) - (hour * 60 * 60);
+
+            if(second < 10)
+                s_second = "0" + second;
+            else
+                s_second = String.valueOf(second);
+
+            String s_min = "";
+            if(min < 10)
+                s_min = "0" + min;
+            else
+                s_min = String.valueOf(second);
+
+            String s_hour = "";
+            if(hour < 10)
+                s_hour = "0" + hour;
+            else
+                s_hour = String.valueOf(hour);
+
+            tv_timer.setText(s_hour + ":" + s_min + ":" + s_second);
+        }
     }
 }
